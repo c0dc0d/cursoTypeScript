22955a8f148e7ac5116670b33397ba9da9c6e2d0
Igor Freitas <igor.soares.de.freitas@gmail.com>
2017-02-28 15:38:48 -0300

    node

--- 01-cadastro-contatos/app/contatos/contatos-mock.js
+++ 01-cadastro-contatos/app/contatos/contatos-mock.js
@@ -1,8 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.CONTATOS = [
     { id: 1, nome: 'Fulano de Tal1', email: 'fulano1@email.com', telefone: '(10) 0000-0000' },
-    { id: 1, nome: 'Fulano de Tal2', email: 'fulano2@email.com', telefone: '(20) 0000-0000' },
-    { id: 1, nome: 'Fulano de Tal3', email: 'fulano3@email.com', telefone: '(30) 0000-0000' },
-    { id: 1, nome: 'Fulano de Tal4', email: 'fulano4@email.com', telefone: '(40) 0000-0000' }
+    { id: 2, nome: 'Fulano de Tal2', email: 'fulano2@email.com', telefone: '(20) 0000-0000' },
+    { id: 3, nome: 'Fulano de Tal3', email: 'fulano3@email.com', telefone: '(30) 0000-0000' },
+    { id: 4, nome: 'Fulano de Tal4', email: 'fulano4@email.com', telefone: '(40) 0000-0000' }
 ];
 //# sourceMappingURL=contatos-mock.js.map
