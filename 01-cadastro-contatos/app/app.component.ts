46fcacee6d988e5fe150fe407bef6cfb570ae08f
Igor Freitas <igor.soares.de.freitas@gmail.com>
2017-02-28 16:00:33 -0300

    teste 3

--- 01-cadastro-contatos/app/app.component.ts
+++ 01-cadastro-contatos/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>My first Angular 2 App Teste 2</h1>
+        <h1>My first Angular 2 App</h1>
 
         <contatos-lista></contatos-lista>
     `
