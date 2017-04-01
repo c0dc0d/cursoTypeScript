1e95916c5f0b58ce650397351d782297531d1e9f
Igor Freitas <igor.soares.de.freitas@gmail.com>
2017-04-01 19:20:40 -0300

    Requisição Http

--- 01-cadastro-contatos/app/contatos/contato.service.js
+++ 01-cadastro-contatos/app/contatos/contato.service.js
@@ -5,14 +5,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var contatos_mock_1 = require("./contatos-mock");
+var http_1 = require("@angular/http");
+require("rxjs/add/operator/toPromise");
 var ContatoService = (function () {
-    function ContatoService() {
+    function ContatoService(http) {
+        this.http = http;
+        this.contatosUrl = "app/contatos";
     }
+    ;
     ContatoService.prototype.getContatos = function () {
-        return Promise.resolve(contatos_mock_1.CONTATOS);
+        return this.http.get(this.contatosUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; });
     };
     ContatoService.prototype.getContato = function (id) {
         return this.getContatos()
@@ -45,7 +54,8 @@ var ContatoService = (function () {
     return ContatoService;
 }());
 ContatoService = __decorate([
-    core_1.Injectable()
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http])
 ], ContatoService);
 exports.ContatoService = ContatoService;
 //# sourceMappingURL=contato.service.js.map
--- 01-cadastro-contatos/app/contatos/contato.service.ts
+++ 01-cadastro-contatos/app/contatos/contato.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
+import 'rxjs/add/operator/toPromise';
+
 import { Contato } from './contato.model';
 import { CONTATOS } from './contatos-mock';
 
 @Injectable()
 export class ContatoService{
 
+    private contatosUrl: string = "app/contatos";
+
+    constructor(
+        private http: Http
+    ){};
+
     getContatos(): Promise<Contato[]> {
-        return Promise.resolve(CONTATOS);
+        return this.http.get(this.contatosUrl)
+            .toPromise()
+            .then(response => response.json().data as Contato[]);
     }
 
     getContato(id: number): Promise<Contato> {
         return this.getContatos()
             .then((contatos: Contato[]) => contatos.find(contato => contato.id === id));
     }
+
+
+
+    
 
     getContatosSloly(): Promise<Contato[]> {
         return new Promise((resolve, reject) => {
